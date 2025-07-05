78b0adf1ea796d3d7d6eb674a312fa13176be8ba
Attila Sragli <sragli.attila@gmail.com>
2025-07-05 15:19:26 +0200

    Test fixes

--- test/bdm_test.exs
+++ test/bdm_test.exs
@@ -1,7 +1,16 @@
 defmodule BdmTest do
   use ExUnit.Case
 
-  test "works correctly for 2x2 matrices" do
+  test "works correctly for lists" do
+    bdm = BDM.new(1, 2)
+    m = [0, 1, 0, 1, 0, 1]
+
+    assert BDM.compute(bdm, m, 1, :ignore) == 5.169925001442312
+    assert BDM.compute(bdm, m, 2, :ignore) == 5.169962500721156
+    assert BDM.compute(bdm, m, 3, :ignore) == 10.34
+  end
+
+  test "works correctly for 2d matrices" do
     bdm = BDM.new(2, 2)
     m = [
       [0, 1, 0, 1, 0, 1],
@@ -11,32 +20,9 @@ defmodule BdmTest do
       [0, 1, 0, 1, 0, 1],
       [1, 0, 1, 0, 1, 0]
     ]
+
     assert BDM.compute(bdm, m, 2, :ignore) == 9.339925001442312
-  end
-
-  test "works correctly for 3x3 matrices" do
-    bdm = BDM.new(2, 2)
-    m = [
-      [0, 1, 0, 1, 0, 1],
-      [1, 0, 1, 0, 1, 0],
-      [0, 1, 0, 1, 0, 1],
-      [1, 0, 1, 0, 1, 0],
-      [0, 1, 0, 1, 0, 1],
-      [1, 0, 1, 0, 1, 0]
-    ]
     assert BDM.compute(bdm, m, 3, :ignore) == 14.34
-  end
-
-  test "works correctly for 4x4 matrices" do
-    bdm = BDM.new(2, 2)
-    m = [
-      [0, 1, 0, 1, 0, 1],
-      [1, 0, 1, 0, 1, 0],
-      [0, 1, 0, 1, 0, 1],
-      [1, 0, 1, 0, 1, 0],
-      [0, 1, 0, 1, 0, 1],
-      [1, 0, 1, 0, 1, 0]
-    ]
     assert BDM.compute(bdm, m, 4, :ignore) == 6.585
   end
 end
